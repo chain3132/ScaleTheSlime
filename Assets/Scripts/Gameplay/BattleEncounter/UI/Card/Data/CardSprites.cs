6c8467261525cf17d85545e97c84b569c0587a71
chain3132 <148683922+chain3132@users.noreply.github.com>
2026-06-05 15:25:22 +0700

    Install NaughtyAttributes

--- Assets/Scripts/Gameplay/BattleEncounter/UI/Card/Data/CardSprites.cs
+++ Assets/Scripts/Gameplay/BattleEncounter/UI/Card/Data/CardSprites.cs
@@ -7,7 +7,9 @@ namespace Gameplay.BattleEncounter.UI.Card
     public class CardSprites 
     {
         public Sprite CardBackground;
+        public Sprite CardArt;
         public Sprite CardHeader;
+        public bool IsSpecialCard; 
         public Sprite CardIcon;
         public Sprite CardFrameLeft;
         public Sprite CardFrameRight;
