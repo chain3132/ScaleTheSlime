cffaeb151161a65cf16c0488d4b1e010206cb56e
chain3132 <148683922+chain3132@users.noreply.github.com>
2026-06-17 15:23:29 +0700

    Auto stash before rebase of "feature/node-selection-ui" onto "feature/scriptableobject-editor-window"

---
+++ Assets/CustomEditor/TableView/NodeTableView.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace CustomEditor.TableView
+{
+    public class NodeTableView : VisualElement
+    {
+        public NodeTableView()
+        {
+            style.flexGrow = 1;
+        }
+    }
+}
